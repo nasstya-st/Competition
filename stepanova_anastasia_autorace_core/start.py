0d058aa6a01f0be65ff4d9d05b3bb6590e0e8a8a
nasstya-st <nastasapervaa@gmail.com>
2023-12-12 17:31:11 +0700

    new structure

--- stepanova_anastasia_autorace_core/start.py
+++ stepanova_anastasia_autorace_core/start.py
@@ -11,73 +11,17 @@ import matplotlib.pyplot as plt
 
 from .sign_recognition import lab, recognition
 
-class Starter(Node):
+#from stepanova_anastasia_autorace_core.action import AvoidBlocks
 
-    def __init__(self):
-        super().__init__('publisher')
-        self.publisher = self.create_publisher(Twist, '/cmd_vel', 1)
-        self.subscription = self.create_subscription(Empty, "robot_start",
-         	    self.listener_callback, 5)
-        self.subscription = self.create_subscription(Image, "/color/image_projected_compensated",
-         	    self.image_callback, 1) 	    
-        self.starter = self.create_subscription(Image, "/color/image",
-         	    self.traffic_light_callback, 1) 
-        self.recognizer = self.create_subscription(Image, "/color/image",
-         	    self.recognizer_callback, 1) 
-         	    
-        self.timer_period = 0.2
-        self.error = [0,0]
-        self.timer = self.create_timer(self.timer_period, self.timer_callback)
+class PID():
+    def __init__(self, timer_period):
         self.prevpt1 = [100,100]
         self.prevpt2 = [700,100]
+        self.timer_period = timer_period
+        self.error = [0,0]
         self.curr_time = 0
         
-        self.lab_data = lab()
-        #self.get_logger.info(f'{self.lab_data[-1]}')
-        
-        self.is_started = 0
-        
-        
-    def traffic_light_callback(self, msg):
-        cv_bridge = CvBridge()
-        frame = cv_bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
-        if not self.is_started:
-            if frame[300, 600][1]==109: self.is_started = 1
-
-
-    def timer_callback(self):
-        if not self.is_started: return
-        self.curr_time += self.timer_period
-        cmd_vel = Twist()
-        cmd_vel.linear.x = 0.1
-        cmd_vel.angular.z = float( \
-        	            0.0125 * self.error[-1] + \
-                            0.000 * np.sum(np.array(self.error)*self.timer_period) + \
-                            0.014 * (self.error[-1] - self.error[-2]) / self.timer_period )
-                            
-        
-        #self.get_logger().info(f'{cmd_vel.angular.z}')
-        self.publisher.publish(cmd_vel)
-
-  
-    def listener_callback(self, msg):
-        self.is_started = 1
-        
-    def recognizer_callback(self, msg):
-        cv_bridge = CvBridge()
-        frame = cv_bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
-        rec_signs = recognition(frame, *self.lab_data)
-        #self.get_logger().info(f'{rec_signs}')
-        
-    def image_callback(self, msg):
-        if not self.is_started: return
-        
-        cv_bridge = CvBridge()
-        gray = cv_bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
-        
-        #rec_signs = recognition(gray, *self.lab_data)
-        #self.get_logger().info(f'{rec_signs}')
-        
+    def calc_error(self, img):
         b1 = 232
         g1 = 0
         r1 = 0
@@ -87,7 +31,7 @@ class Starter(Node):
         h_min = (g1, b1, r1)
         h_max = (g2, b2, r2)
 
-        gray = cv2.inRange(gray, h_min, h_max)
+        gray = cv2.inRange(img, h_min, h_max)
 
         dst = gray[int(gray.shape[0]/3*2):, :]
         
@@ -125,6 +69,75 @@ class Starter(Node):
         fpt = [(cpt[0][0] + cpt[1][0]) / 2, (cpt[0][1] + cpt[1][1]) / 2 + gray.shape[0] / 3 * 2]
         
         self.error.append(dst.shape[1] / 2 - fpt[0])
+    	
+        
+    def update_error(self):
+        self.curr_time += self.timer_period
+        Kp, Ki, Kd = (0.0185, 0.0, 0.019)
+        cmd_vel = Twist()
+        cmd_vel.linear.x = 0.15
+        cmd_vel.angular.z = float( \
+        	            Kp * self.error[-1] + \
+                            Ki * np.sum(np.array(self.error)*self.timer_period) + \
+                            Kd * (self.error[-1] - self.error[-2]) / self.timer_period )
+        return cmd_vel
+        
+
+class Starter(Node):
+
+    def __init__(self):
+        super().__init__('publisher')
+        self.publisher = self.create_publisher(Twist, '/cmd_vel', 1)
+        self.subscription = self.create_subscription(Empty, "robot_start",
+         	    self.empty_listener_callback, 5)
+        self.subscription = self.create_subscription(Image, "/color/image_projected_compensated",
+         	    self.image_callback, 1) 	    
+        self.starter = self.create_subscription(Image, "/color/image",
+         	    self.traffic_light_callback, 1) 
+        self.recognizer = self.create_subscription(Image, "/color/image", self.recognizer_callback, 1) 
+         	    
+        #self.avoid_blocks = ActionClient(self, AvoidBlocks, 'execute_turtle_commands')
+         	    
+        self.timer_period = 0.2
+        self.timer = self.create_timer(self.timer_period, self.timer_callback)
+        self.pid = PID(self.timer_period)
+        
+        self.lab_data = lab()
+        #self.get_logger.info(f'{self.lab_data[-1]}')
+        
+        self.is_started = 0
+        
+        
+    def traffic_light_callback(self, msg):
+        cv_bridge = CvBridge()
+        frame = cv_bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+        if not self.is_started:
+            if frame[300, 600][1]==109: self.is_started = 1
+
+    def timer_callback(self):
+        if not self.is_started: return
+        new_vel = self.pid.update_error()
+        self.publisher.publish(new_vel)
+  
+    def empty_listener_callback(self, msg):
+        self.is_started = 1
+        
+    def recognizer_callback(self, msg):
+        cv_bridge = CvBridge()
+        frame = cv_bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+        rec_signs = recognition(frame, *self.lab_data)
+        #self.get_logger().info(f'{rec_signs}')
+        
+    def image_callback(self, msg):
+        if not self.is_started: return
+        
+        cv_bridge = CvBridge()
+        image = cv_bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+        
+        self.pid.calc_error(image)
+        #rec_signs = recognition(gray, *self.lab_data)
+        #self.get_logger().info(f'{rec_signs}')
+        
         
         
 def main(args=None):
